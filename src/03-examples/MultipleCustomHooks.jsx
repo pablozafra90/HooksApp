4831df57026d95d158def6ec39365b4795920b05
Pablo <pablozafra90@hotmail.com>
2023-08-17 21:52:57 +0200

    127 done

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -2,14 +2,34 @@ import { useFetch } from "../hooks/useFetch"
 
 export const MultipleCustomHooks = () => {
 
-    const { data, isLoading, hasError } = useFetch('https://api.breakingbadquotes.xyz/v1/quotes/4');
+    const { data, isLoading, hasError } = useFetch('https://api.breakingbadquotes.xyz/v1/quotes/1');
 
     console.log({ data, isLoading, hasError });
 
-  return (
-    <>
-        <h1>Breaking Bad Quotes</h1>
-        <hr />
-    </>
-  )
+    const { quote, author } = !!data && data[0]
+
+    return (
+        <>
+            <h1>Breaking Bad Quotes</h1>
+            <hr />
+
+            { isLoading 
+            ? (
+                <div className="alert alert-info text-center">
+                    Loading...
+                </div>
+            )
+            : (
+                <>
+                    <blockquote className="blockquote text-right text-end">
+                        <p className="mb-1">{ quote }</p>
+                        <footer className="blockquote-footer">{ author }</footer>
+                    </blockquote>
+
+                    <button className="btn btn-primary">Next Quote</button>
+                </>
+            )} 
+        </>
+    )
+
 }

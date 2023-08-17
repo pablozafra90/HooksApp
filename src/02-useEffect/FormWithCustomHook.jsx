70b40e11c73d6136cf65d54eea27584a100bab9c
Pablo <pablozafra90@hotmail.com>
2023-08-17 19:20:48 +0200

    124 completada

---
+++ src/02-useEffect/FormWithCustomHook.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from 'react';
+import { Message } from './Message';
+import { useForm } from '../hooks/useForm';
+
+export const FormWithCustomHook = () => {
+
+    // const [formState, setFormState] = useState({
+    //     username: '',
+    //     email: '',
+    //     password: '',
+    // })
+
+    // const { username, email, password } = formState;
+
+    // const onInputChange = ( { target } ) => {
+    //     const { name, value } = target;
+    //     setFormState({
+    //         ...formState,
+    //         [ name ]: value,
+    //     })
+    //     // console.log( name )
+    // }
+
+    const { formState, onInputChange } = useForm({
+        username: '',
+        email: '',
+        password: '',
+    });
+
+    const { username, email, password } = formState;
+
+    return (
+        <>
+            <h1>Formulario simple</h1>
+            <hr />
+
+            <input 
+                type="text"
+                className="form-control"
+                placeholder="Username"
+                name="username"
+                value={ username }
+                onChange={ onInputChange }
+            />
+
+            <input 
+                type="email"
+                className="form-control mt-2"
+                placeholder="ejemplo@pablozafra.es"
+                name="email"
+                value={ email }
+                onChange={ onInputChange }
+            />
+
+            <input 
+                type="password"
+                className="form-control mt-2"
+                placeholder="contraseña"
+                name="password"
+                value={ password }
+                onChange={ onInputChange }
+            />
+            
+            { (username === "a") && <Message/>}
+        </>
+  )
+}
---
+++ src/Hooks/useForm.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+export const useForm = ( initialForm = {}) => {
+
+    const [formState, setFormState] = useState( initialForm );
+
+    const onInputChange = ( { target } ) => {
+        const { name, value } = target;
+        setFormState({
+            ...formState,
+            [ name ]: value,
+        })
+        // console.log( name )
+    }
+    
+    return {
+        formState,
+        onInputChange,
+    };
+}

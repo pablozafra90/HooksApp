490ae938f586b3824860146d21e1257e0301ca44
pablozafra90 <pablozafra90@hotmail.com>
2024-02-19 16:23:39 +0100

    inicioRouters

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
 import './style.css';
 // import { FocusScreen } from './04-useRef/FocusScreen';
 // import { Memorize } from './06-memos/Memorize';
@@ -7,10 +8,14 @@ import './style.css';
 // import { Padre } from './07-tarea-memo/Padre';
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 // import './08-useReducer/intro-reducer';
-import { TodoApp } from './08-useReducer/TodoApp';
+// import { TodoApp } from './08-useReducer/TodoApp';
+import { MainApp } from './09-useContext/MainApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  //<React.StrictMode>
-      <TodoApp />
-  //</React.StrictMode>,
+  
+  <BrowserRouter>
+      {/* <React.StrictMode> */}
+          <MainApp />
+      {/* </React.StrictMode>, */}
+    </BrowserRouter>
 )

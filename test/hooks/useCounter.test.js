64cc2e657a568ba522533cd40d7acf89100cb668
pablozafra90 <pablozafra90@hotmail.com>
2024-03-05 19:20:12 +0100

    Jest started

---
+++ test/hooks/useCounter.test.js
@@ -0,0 +1,11 @@
+import { useCounter } from "../../src/Hooks/useCounter";
+
+describe('Pruebas en el useCounter', () => { 
+    
+    test('Debe retornar los valores por defecto', () => { 
+
+        
+        
+     })
+
+ })

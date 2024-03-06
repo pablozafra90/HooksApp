ae32220172ecbca809f6bb46e4de4f72843c9def
pablozafra90 <pablozafra90@hotmail.com>
2024-03-06 17:31:28 +0100

    clase167-completada

--- test/hooks/useCounter.test.js
+++ test/hooks/useCounter.test.js
@@ -1,11 +1,29 @@
+import { renderHook } from "@testing-library/react";
 import { useCounter } from "../../src/Hooks/useCounter";
 
 describe('Pruebas en el useCounter', () => { 
     
     test('Debe retornar los valores por defecto', () => { 
 
-        
+        const { result } = renderHook( () => useCounter() );
+        // console.log( result );
+        const { counter, increment, decrement, reset } =  result.current;
+
+        expect( counter ).toBe(10);
+        expect( increment ).toEqual(expect.any(Function));
+        expect( decrement ).toEqual(expect.any(Function));
+        expect( reset ).toEqual(expect.any(Function));
         
      })
+
+     test('Debe retornar valor de 100', () => {
+
+        const { result } = renderHook( () => useCounter(100) );
+        const { counter } =  result.current;
+        expect( counter ).toBe(100);
+
+        
+     });
+     
 
  })
